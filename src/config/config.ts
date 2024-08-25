0d5362666ae807705b01f8ba533520be4fc5cf8a
jcardenasz <jcardenasz@unal.edu.co>
2024-08-25 00:11:12 -0500

    base connection with database, working

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,14 @@
+// src/config/config.ts
 import dotenv from 'dotenv';
 dotenv.config();
 
 export class Config {
-    private static _PORT: number = parseInt(process.env.PORT || "3000",10);
+    private static _PORT: number = parseInt(process.env.PORT || "3000", 10);
     private static _USERDB: string = process.env.DB_USER || '';
-    private static _HOSTDB: string = "http://localhost:"+Config._PORT|| '';
+    private static _HOSTDB: string = process.env.DB_HOST || 'localhost';
     private static _DATABASE: string = process.env.DB_DATABASE || '';
     private static _PASSWORD: string = process.env.DB_PASSWORD || '';
-
+    private static _DB_PORT: number = parseInt(process.env.DB_PORT || "5432", 10);
 
     static get PORT(): number {
         return this._PORT;
@@ -28,5 +29,8 @@ export class Config {
     static get PASSWORD(): string {
         return this._PASSWORD;
     }
-}
 
+    static get DB_PORT(): number {
+        return this._DB_PORT;
+    }
+}
